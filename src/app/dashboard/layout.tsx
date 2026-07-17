1e6494d850ee9976b8f683279b97899f64e0468d
shamim-hasan259 <mdshamimhasan791@gmail.com>
2026-07-17 12:10:04 +0600

    sidebar user erro fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,10 +3,11 @@ import { getUserSession } from "@/lib/user/user";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const user = await getUserSession();
+  const sidebarUser = user ? { ...user, userId: user.id } : null;
   return (
     <div className="grid grid-cols-12 gap-8 py-8 ">
       <div className="col-span-12 md:col-span-3 lg:col-span-2">
-        <Sidebar user={user} />
+        <Sidebar user={sidebarUser} />
       </div>
       <main className="col-span-12 md:col-span-9 lg:col-span-10">
         {children}
